026e15a236b33964e02b62a11c09cf62f42311c3
Ovchinnikov Alexey <gdhald4@gmail.com>
2024-05-13 13:19:37 +0500

    aboutPageAdd

--- frontend/src/Root/root.jsx
+++ frontend/src/Root/root.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import MainPage from '../pages/mainPage';
 import NotFoundPage from '../pages/notFoundPage';
+import AboutPage from '../pages/aboutPage';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import './root.css';
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="*" element={<NotFoundPage />} />
+      <Route path="about" element={<AboutPage />}/>
     </Routes>
   </BrowserRouter>
 );
---
+++ frontend/src/pages/aboutPage.jsx
@@ -0,0 +1,13 @@
+import style from './pagesMainStyle/aboutPage.module.css'
+
+function AboutPage() {
+    return ( 
+        <>
+            <div className={style.Content}>
+                123
+            </div>
+        </>
+     );
+}
+
+export default AboutPage;
