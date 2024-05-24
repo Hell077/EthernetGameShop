4035ee998586fc0795c9ae72a0ed6b33ca751745
Ovchinnikov Alexey <gdhald4@gmail.com>
2024-05-24 16:37:15 +0500

    woow

--- frontend/src/modules/ReactSlickTest/SlickTest.jsx
+++ frontend/src/modules/ReactSlickTest/SlickTest.jsx
@@ -7,7 +7,7 @@ import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 function Slick() {
     const slides = [
-        { img: "", text: 'Slide 1 Description', price: '$49.99' },
+        { img: "https://source.unsplash.com/random/1800x700?sig=1", text: 'Slide 1 Description', price: '$49.99' },
         { img: "https://source.unsplash.com/random/1800x700?sig=2", text: 'Slide 2 Description', price: '$49.99' },
         { img: "https://source.unsplash.com/random/1800x700?sig=3", text: 'Slide 3 Description', price: '$59.99' },
         { img: "https://source.unsplash.com/random/1800x700?sig=4", text: 'Slide 4 Description', price: '$79.99' },
