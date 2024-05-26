19ea87854032e6527e8494e97c8dd40a6732aa0a
Alexey Ovchinnikov <gdhald4@gmail.com>
2024-05-26 23:10:33 +0500

    add popUp

--- frontend/src/modules/Header/header.jsx
+++ frontend/src/modules/Header/header.jsx
@@ -1,32 +1,33 @@
 import styles from './header.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCartShopping, faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { useState } from "react";
 
 function Header() {
-  const [open, setOpen] = useState("none");
+    const [open, setOpen] = useState(false);
 
-    function OpenPopUp (){
-        setOpen(open === "block" ? "none" : "block");
+    function togglePopUp() {
+        setOpen(!open);
     }
-    return (<>
-            <div className={styles.PopUpWindow} style={{display:open}}>
-
+    return (
+        <>
+            <div className={`${styles.PopUpWindow} ${open ? styles.open : ''}`}>
+                <FontAwesomeIcon icon={faXmark}  style={{fontSize: '2.5rem'}} onClick={togglePopUp}/>
             </div>
             <header className={styles.header}>
                 <div className={styles.HeaderElements}>
-                    <div className={styles.element} onClick={OpenPopUp}>
-                        <FontAwesomeIcon icon={faBars} style={{fontSize: '2rem'}}/>
+                    <div className={styles.element} onClick={togglePopUp}>
+                        <FontAwesomeIcon icon={faBars} style={{ fontSize: '2rem' }} />
                     </div>
                     <div className={styles.element}>
-                        <FontAwesomeIcon icon={faUser} style={{fontSize: '2rem'}}/>
+                        <FontAwesomeIcon icon={faUser} style={{ fontSize: '2rem' }} />
                     </div>
                     <div className={styles.element}>
-                        <FontAwesomeIcon icon={faCartShopping} style={{fontSize: '2rem'}}/>
+                        <FontAwesomeIcon icon={faCartShopping} style={{ fontSize: '2rem' }} />
                     </div>
                     <div className={styles.element}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} style={{fontSize: '2rem'}}/>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} style={{ fontSize: '2rem' }} />
                     </div>
                 </div>
             </header>
