dae446cd7bd49fd6a7b30af5b92ca919153d3b57
Alexey Ovchinnikov <gdhald4@gmail.com>
2024-06-06 17:02:22 +0500

    add links

--- frontend/src/modules/ReactSlickTest/SlickTest.jsx
+++ frontend/src/modules/ReactSlickTest/SlickTest.jsx
@@ -6,19 +6,19 @@ import PrevArrow from './LeftArrow';
 import NextArrow from './RightArrow';
 
 const SlickButton = ({ path, content }) => (
-  <a href={path} className={styles.slideButton}>
-    {content}
-  </a>
+    <a href={path} className={styles.slideButton}>
+      {content}
+    </a>
 );
 
 function Slick() {
   const slides = [
-    { img: 'https://source.unsplash.com/random/1800x700?sig=1', text: 'Slide 1 Description', price: '$49.99' },
-    { img: 'https://source.unsplash.com/random/1800x700?sig=2', text: 'Slide 2 Description', price: '$49.99' },
-    { img: 'https://source.unsplash.com/random/1800x700?sig=3', text: 'Slide 3 Description', price: '$59.99' },
-    { img: 'https://source.unsplash.com/random/1800x700?sig=4', text: 'Slide 4 Description', price: '$79.99' },
-    { img: 'https://source.unsplash.com/random/1800x700?sig=5', text: 'Slide 5 Description', price: '$89.99' },
-   ];
+    { img: 'https://source.unsplash.com/random/1800x700?sig=1', text: 'Slide 1 Description', price: '$49.99', path: '' },
+    { img: 'https://source.unsplash.com/random/1800x700?sig=2', text: 'Slide 2 Description', price: '$49.99', path: '' },
+    { img: 'https://source.unsplash.com/random/1800x700?sig=3', text: 'Slide 3 Description', price: '$59.99', path: '' },
+    { img: 'https://source.unsplash.com/random/1800x700?sig=4', text: 'Slide 4 Description', price: '$79.99', path: '' },
+    { img: 'https://source.unsplash.com/random/1800x700?sig=5', text: 'Slide 5 Description', price: '$89.99', path: '' },
+  ];
 
   const settings = {
     infinite: true,
@@ -43,31 +43,29 @@ function Slick() {
         },
       },
     ],
-
   };
 
   return (
-    <div className={styles.sliderWrapper}>
-      <div className={styles.sliderContainer}>
-        <div className={styles.slider}>
-          <Slider {...settings}>
-            {slides.map((slide, index) => (
-              <div key={index} className={styles.slide}>
-                <SlickButton path="" content={<img src={slide.img} alt={`Slide ${index + 1}`} />} />
-                <div className={styles.infoBox}>
-                  <span className={styles.text}>{slide.text}</span>
-                  <span className={styles.price}>{slide.price}</span>
-                </div>
-              </div>
-            ))}
-          </Slider>
+      <div className={styles.sliderWrapper}>
+        <div className={styles.sliderContainer}>
+          <div className={styles.slider}>
+            <Slider {...settings}>
+              {slides.map((slide, index) => (
+                  <div key={index} className={styles.slide}>
+                    <SlickButton path={slide.path} content={<img src={slide.img} alt={`Slide ${index + 1}`} />} />
+                    <div className={styles.infoBox}>
+                      <span className={styles.text}>{slide.text}</span>
+                      <span className={styles.price}>{slide.price}</span>
+                    </div>
+                  </div>
+              ))}
+            </Slider>
+          </div>
         </div>
+        <PrevArrow className={styles.slickPrev} />
+        <NextArrow className={styles.slickNext} />
       </div>
-      <PrevArrow className={styles.slickPrev} />
-      <NextArrow className={styles.slickNext} />
-    </div>
   );
 }
 
 export default Slick;
-    
--- frontend/src/modules/topGamesList/list/list.jsx
+++ frontend/src/modules/topGamesList/list/list.jsx
@@ -16,24 +16,21 @@ import horizonzero from './listImages/horizonzero.jpg'
 import nomanssky from './listImages/nomanssky.jpg'
 import metroexodus from './listImages/metroexodus.jpg'
 
-//when installing a new component, set the props value for key to 1,2,3 like this
-
-//  <MassiveComp key={1} tagH={"Manor Lords"} tagP={'Описание Manor Lords'} image={manorlords} />,
 
 const newsData = {
     'Новинки': [
-        <MassiveComp path="" key={1} tagH={"Manor Lords"} tagP={'Manor Lords - это стратегическая игра о строительстве и управлении средневековым поселением.'} image={manorlords} />,
-        <MassiveComp path="" key={2} tagH={"Dota2"} tagP={' Dota 2 - онлайн-игра с героями, цель которой - захватить базу противника.'} image={dota2} />,
-        <MassiveComp path="" key={3} tagH={"Helldivers 2"} tagP={'Helldivers 2 - это кооперативный шутер с видом сверху, где игроки сражаются против враждебных сил в космическом мире.'} image={helldrivers} />,
-        <MassiveComp path="" key={4} tagH={"Destiny 2 The Final Shape"} tagP={'Destiny 2: The Final Shape - это расширение для игры Destiny 2, которое предлагает новые миры и задания в борьбе за судьбу вселенной.'} image={destiny2} />,
-        <MassiveComp path="" key={5} tagH={"Hexguardian"} tagP={'Hexguardian - это фэнтезийная игра с элементами стратегии, в которой игроки сражаются с монстрами и исследуют мир, используя магические способности.'} image={hexguardian} />,
+        <MassiveComp path="http://localhost:5173/catalog/665ce1c65b7d584f9a5ad462" key={1} tagH={"Manor Lords"} tagP={'Manor Lords - это стратегическая игра о строительстве и управлении средневековым поселением.'} image={manorlords} />,
+        <MassiveComp path="http://localhost:5173/catalog/665cd2915b7d584f9a5ad44a" key={2} tagH={"Dota2"} tagP={' Dota 2 - онлайн-игра с героями, цель которой - захватить базу противника.'} image={dota2} />,
+        <MassiveComp path="http://localhost:5173/catalog/665ce1cb5b7d584f9a5ad463" key={3} tagH={"Helldivers 2"} tagP={'Helldivers 2 - это кооперативный шутер с видом сверху, где игроки сражаются против враждебных сил в космическом мире.'} image={helldrivers} />,
+        <MassiveComp path="http://localhost:5173/catalog/665ce1cf5b7d584f9a5ad464" key={4} tagH={"Destiny 2 The Final Shape"} tagP={'Destiny 2: The Final Shape - это расширение для игры Destiny 2, которое предлагает новые миры и задания в борьбе за судьбу вселенной.'} image={destiny2} />,
+        <MassiveComp path="http://localhost:5173/catalog/665ce1d45b7d584f9a5ad465" key={5} tagH={"Hexguardian"} tagP={'Hexguardian - это фэнтезийная игра с элементами стратегии, в которой игроки сражаются с монстрами и исследуют мир, используя магические способности.'} image={hexguardian} />,
     ],
     'Лидеры продаж': [
-        <MassiveComp path="" key={6} tagH={"GTFO"} tagP={'GTFO - это кооперативный шутер с элементами выживания, где игроки исследуют подземелья, борясь с враждебными существами в атмосфере напряжения и ужаса.'} image={gtfo} />,
-        <MassiveComp path="" key={7} tagH={"Hell Let Loose"} tagP={'Hell Let Loose - это многопользовательская военная игра, в которой игроки принимают участие в реалистичных сражениях времен Второй мировой войны.'} image={hellletloose} />,
-        <MassiveComp path="" key={8} tagH={"Horizon Zero Dawn™ Complete Edition"} tagP={'Horizon Zero Dawn: Complete Edition - приключенческая игра в открытом мире с боевой системой против механизированных существ в постапокалиптическом мире.'} image={horizonzero} />,
-        <MassiveComp path="" key={9} tagH={"No Man's Sky"} tagP={'No Mans Sky - это открытая вселенная, где игроки исследуют бесконечные галактики, обнаруживают разнообразные планеты и существ, строят базы и путешествуют по космосу.'} image={nomanssky} />,
-        <MassiveComp path="" key={10} tagH={"Metro Exodus"} tagP={'"Метро: Исход" - шутер с открытым миром, где игроки путешествуют по постапокалиптической России.'} image={metroexodus} />,
+        <MassiveComp path="http://localhost:5173/catalog/665cdb795b7d584f9a5ad45c" key={6} tagH={"GTFO"} tagP={'GTFO - это кооперативный шутер с элементами выживания, где игроки исследуют подземелья, борясь с враждебными существами в атмосфере напряжения и ужаса.'} image={gtfo} />,
+        <MassiveComp path="http://localhost:5173/catalog/665cdb7d5b7d584f9a5ad45d" key={7} tagH={"Hell Let Loose"} tagP={'Hell Let Loose - это многопользовательская военная игра, в которой игроки принимают участие в реалистичных сражениях времен Второй мировой войны.'} image={hellletloose} />,
+        <MassiveComp path="http://localhost:5173/catalog/665cdb875b7d584f9a5ad45f" key={8} tagH={"Horizon Zero Dawn™ Complete Edition"} tagP={'Horizon Zero Dawn: Complete Edition - приключенческая игра в открытом мире с боевой системой против механизированных существ в постапокалиптическом мире.'} image={horizonzero} />,
+        <MassiveComp path="http://localhost:5173/catalog/665cdb8e5b7d584f9a5ad460" key={9} tagH={"No Man's Sky"} tagP={'No Mans Sky - это открытая вселенная, где игроки исследуют бесконечные галактики, обнаруживают разнообразные планеты и существ, строят базы и путешествуют по космосу.'} image={nomanssky} />,
+        <MassiveComp path="http://localhost:5173/catalog/665cdb9b5b7d584f9a5ad461" key={10} tagH={"Metro Exodus"} tagP={'"Метро: Исход" - шутер с открытым миром, где игроки путешествуют по постапокалиптической России.'} image={metroexodus} />,
     ],
 };
 
